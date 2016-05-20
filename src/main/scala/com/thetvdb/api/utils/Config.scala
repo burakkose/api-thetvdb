dd9e95caf6bf25aa5be4187ade89cf2ec1a6f263
Burak Köse <burakks41@gmail.com>
2016-05-21 00:02:16 +0300

    add configs

--- src/main/scala/com/thetvdb/api/utils/Config.scala
+++ src/main/scala/com/thetvdb/api/utils/Config.scala
@@ -7,5 +7,13 @@ private[utils] trait Config {
 }
 
 trait ApiConfig extends Config {
-  // private val apiConfig = config.getConfig("http")
+  private val apiConfig = config.getConfig("api")
+
+  protected val SERIES_URL = apiConfig.getString("series")
+  protected val SEARCH_SERIES_URL = apiConfig.getString("searchSeries")
+  protected val LANGUAGE_URL = apiConfig.getString("language")
+  protected val REFRESH_TOKEN_URL = apiConfig.getString("refreshToken")
+  protected val LOGIN_URL = apiConfig.getString("login")
+  protected val EPISODE_URL = apiConfig.getString("episode")
+  protected val UPDATE_URL = apiConfig.getString("update")
 }
